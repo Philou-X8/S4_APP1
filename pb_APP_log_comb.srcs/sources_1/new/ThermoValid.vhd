a63ba028565f30861131ce22ad93db7354318fb4
Philou-X8 <pdurettesds@gmail.com>
2024-01-16 15:50:07 -0500

    unused code

--- pb_APP_log_comb.srcs/sources_1/new/ThermoValid.vhd
+++ pb_APP_log_comb.srcs/sources_1/new/ThermoValid.vhd
@@ -38,49 +38,8 @@ end ThermoValid;
 
 architecture Behavioral of ThermoValid is
     
-    
-    component Add4bits 
-        port (
-            aVect : in STD_LOGIC_VECTOR(3 downto 0);
-            bVect : in STD_LOGIC_VECTOR(3 downto 0);
-            cin : in STD_LOGIC;
-            sVect : out STD_LOGIC_VECTOR(3 downto 0);
-            cout : out STD_LOGIC
-        );
-    end component;
-    
-    signal carry1 : std_logic;
-    signal carry2 : std_logic;
-    signal carry3 : std_logic;
-    signal sum_vect : std_logic_vector(11 downto 0) := "000000000000";
-    signal adc12_neg : std_logic_vector(11 downto 0) := "111111111111";
-
 begin
-    adc12_neg <= adc12_in XOR "111111111111";
-    adder1 : Add4bits
-    port map (
-        aVect => adc12_in(3 downto 0),
-        bVect => adc12_neg(3 downto 0),
-        cin => '1',
-        sVect => sum_vect(3 downto 0),
-        cout => carry1
-    );
-    adder2 : Add4bits
-    port map (
-        aVect => adc12_in(7 downto 4),
-        bVect => adc12_neg(7 downto 4),
-        cin => carry1,
-        sVect => sum_vect(7 downto 4),
-        cout => carry2
-    );
-    adder3 : Add4bits
-    port map (
-        aVect => adc12_in(11 downto 8),
-        bVect => adc12_neg(11 downto 8),
-        cin => carry2,
-        sVect => sum_vect(11 downto 8),
-        cout => carry3
-    );
+    
     err_out <= 
         ( adc12_in(1) AND (NOT adc12_in(0)) ) OR
         ( adc12_in(2) AND (NOT adc12_in(1)) ) OR
@@ -94,9 +53,4 @@ begin
         ( adc12_in(10) AND (NOT adc12_in(9)) ) OR
         ( adc12_in(11) AND (NOT adc12_in(10)) ) ;
         
-    
-    
-    --err_out <= NOT carry3;
-
-
 end Behavioral;
